6d94537b4ede3e32e8a4c699725e5f42df84539b
David Mininger <dmininger+github@gmail.com>
2016-04-12 15:11:59 -0500

    Improve test for buildSelector and add function documentation.

--- source/_test/buildSelector_test.js
+++ source/_test/buildSelector_test.js
@@ -1,6 +1,26 @@
 const test = require('tape');
 const buildSelector = require('../buildSelector');
 
+test('buildSelector', assert => {
+  const queries = { query1: 'FAKE1', query2: 'FAKE2' };
+  const recordsProperty = 'FAKE3';
+  const selectRecords = (groupedRecords, query) => `${groupedRecords}-${query}`;
+
+  const state = {
+    'FAKE3': 'FAKE4',
+  };
+
+  const selector = buildSelector(queries, recordsProperty, selectRecords);
+
+  const actual = selector(state);
+  const expected = {
+    query1: 'FAKE4-FAKE1',
+    query2: 'FAKE4-FAKE2',
+  };
+
+  assert.deepEqual(actual, expected, 'returns results of queries in correct shape');
+  assert.end();
+});
 
 // TODO can we use randomized property based testing here?
 test('buildSelector', assert => {
--- source/buildSelector.js
+++ source/buildSelector.js
@@ -2,14 +2,28 @@ import entries from 'object.entries';
 
 import _selectRecords from './selectRecords';
 
+/*
+  # buildSelector
+  Returns a function that, when applied to a state object, returns an object
+  with the same shape as the queries param, but with each property value the
+  result of its respective query.
+
+  @param {Object} queries - An object with queries as property values
+  @param {String} recordsProperty - name of the property in your state object containing records
+  @param {Function} selectRecords - used to create a selector function. TODO more details
+  @return {Function} - When applied to state object returns result of queries
+  @example TODO
+
+  TODO: check types (dev only for better error messages)
+*/
 export default function buildSelector(
   queries,
-  recordsObject,
+  recordsProperty,
   selectRecords = _selectRecords
 ) {
   return state => {
     return entries(queries).reduce((namedSelectors, [queryName, query]) => {
-      return { [queryName]: selectRecords(state[recordsObject], query), ...namedSelectors };
+      return { [queryName]: selectRecords(state[recordsProperty], query), ...namedSelectors };
     }, {});
   };
 }
