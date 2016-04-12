c256aca9d3dd9bcba6a86478808a8ed1d8e12387
David Mininger <dmininger+github@gmail.com>
2016-04-12 13:37:19 -0500

    Extract selectRecords from buildSelector.

--- source/buildSelector.js
+++ source/buildSelector.js
@@ -1,32 +1,14 @@
 import entries from 'object.entries';
-import pipe from 'lodash.flow';
-import curry from 'lodash.curry';
 
+import _selectRecords from './selectRecords';
 
-export default function buildSelector(queries) {
+export default function buildSelector(
+  queries,
+  selectRecords = _selectRecords
+) {
   return state => {
     return entries(queries).reduce((namedSelectors, [queryName, query]) => {
       return { [queryName]: selectRecords(state, query), ...namedSelectors };
     }, {});
   };
-}
-
-const target = curry((target, recordGroups) => recordGroups[target]);
-
-const select = curry((selectedFields, records) => (
-  records.map(record => {
-    return selectedFields.reduce((fields, fieldName) => {
-      return { [fieldName]: record[fieldName], ...fields };
-    }, {});
-  })
-));
-
-const limit = curry((limit, records) => records.slice(0, limit));
-
-function selectRecords(state, query) {
-  return pipe(
-    target(query.target),
-    select(query.select),
-    limit(query.limit)
-  )(state._locus_records);
 }
---
+++ source/selectRecords.js
@@ -0,0 +1,24 @@
+import pipe from 'lodash.flow';
+import curry from 'lodash.curry';
+
+const target = curry(
+  (target, recordsGroupedByType) => recordsGroupedByType[target]
+);
+
+const select = curry((selectedFields, records) => (
+  records.map(record => {
+    return selectedFields.reduce((fields, fieldName) => {
+      return { [fieldName]: record[fieldName], ...fields };
+    }, {});
+  })
+));
+
+const limit = curry((limit, records) => records.slice(0, limit));
+
+export default function selectRecords(state, query) {
+  return pipe(
+    target(query.target),
+    select(query.select),
+    limit(query.limit)
+  )(state._locus_records);
+}
