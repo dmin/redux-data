35279390c2ea780cbc1a21f2368e7568e06f9f06
David Mininger <dmininger+github@gmail.com>
2016-04-13 16:17:11 -0500

    Refactor variable name in locusRecordsReducer.

--- source/locusRecordsReducer.js
+++ source/locusRecordsReducer.js
@@ -1,10 +1,10 @@
-export default function (records = {}, action) {
+export default function (recordsGroupedByType = {}, action) {
   switch (action.type) {
     case 'RECEIVE_REMOTE_RECORDS':
       // TODO more sophisticated merging
       // TODO performance - persistant data structure would be nice here
-      return { [action.target]: action.records, ...records };
+      return { [action.target]: action.records, ...recordsGroupedByType };
     default:
-      return records;
+      return recordsGroupedByType;
   }
 }
