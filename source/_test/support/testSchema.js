7b7befe4c37d620d072f1ab0e04e17436031f8eb
David Mininger <dmininger+github@gmail.com>
2016-04-19 20:07:34 -0500

    Create schema to be used for testing.

---
+++ source/_test/support/testSchema.js
@@ -0,0 +1,14 @@
+/*
+  TODO: how should this be kept up to date and validated?
+*/
+
+export default {
+
+  items: {
+    fields: [
+      { name: 'id', type: String },
+      { name: 'name', type: String },
+      { name: 'quantity', type: Number },
+    ],
+  },
+};
