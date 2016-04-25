9c7ba4643c9d8d83d60e82f5e1a8a01d76d913b4
David Mininger <dmininger+github@gmail.com>
2016-04-25 15:58:27 -0500

    Rework warning when field type not defined in schema to only show once.

--- source/typeCastFields.js
+++ source/typeCastFields.js
@@ -5,12 +5,21 @@ export default function typeCastFields(schema, target, suppliedFields) {
   // TODO extract finding the schemaFields to outside this function
   const schemaFields = schema[target].fields;
 
-  return Object.entries(suppliedFields).reduce((typeCastFields, [fieldName, fieldValue]) => {
+  if (__DEV__) { // TODO requires a __DEV__ global - set by webpack, what about during unit testing?
+    // TODO is this the best way?
+    var warnings = {};
+  }
+
+  const fields = Object.entries(suppliedFields).reduce((typeCastFields, [fieldName, fieldValue]) => {
     const schemaField = find(schemaFields, { name: fieldName });
     if (!schemaField) { // TODO only in dev
       // TODO better way?
       // TODO it would be nice to know if they was happening on data from server / form field / etc
-      console.log(`The "${target}" collection does not have a the field "${fieldName}" specified in the schema. Cannot perform type cast.`);
+
+      if (__DEV__) {
+        warnings[target] = `The "${target}" collection does not have a the field "${fieldName}" specified in the schema. Cannot perform type cast.`;
+      }
+
       return {
         ...typeCastFields,
         [fieldName]: fieldValue,
@@ -21,4 +30,12 @@ export default function typeCastFields(schema, target, suppliedFields) {
       [fieldName]: schemaField.type(fieldValue),
     };
   }, {});
+
+  if (__DEV__) {
+    Object.entries(warnings).forEach(([key, value]) => {
+      console.warn(value);
+    });
+  }
+
+  return fields;
 }
