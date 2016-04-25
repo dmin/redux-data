b6f71f036753cedf5e7d701afd5ed305191188e8
David Mininger <dmininger+github@gmail.com>
2016-04-25 16:27:27 -0500

    Update buildUrl to handle where query property.

--- source/buildUrl.js
+++ source/buildUrl.js
@@ -1,6 +1,10 @@
 export default function buildUrl(query, options) {
 
+  // TODO this is pretty ugly
   const format = options.format ? `.${options.format}` : '';
+  const where = query.where ? options.capabilities.where(query.where) : '';
+  const queryStringPrep = `${where}`;
+  const queryString = queryStringPrep ? `?${queryStringPrep}` : '';
 
-  return `${options.baseUrl}/${options.names.collection}${format}`;
+  return `${options.baseUrl}/${options.names.collection}${format}${queryString}`;
 }
